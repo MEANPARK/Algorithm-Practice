0c026518cca583d7493f7a069c63bd2996e2d1ca
MEANPARK <techrosseo@naver.com>
2025-03-08 21:16:40 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,19 @@
+class Solution {
+     public int solution(int n) { 
+        int cnt = count(n);
+        while(true){
+            if(cnt == count(++n)){
+                return n;
+            }
+        }
+    }
+    public static int count(int input){
+        int cnt = 0;
+        while(input >= 2){
+            if(input % 2 == 1) cnt++;
+            input /= 2;
+        } 
+        if(input == 1) cnt++;
+        return cnt;
+    }
+}
