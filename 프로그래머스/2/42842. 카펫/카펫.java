4fc6a32f5b0c1bd8427f742f212f0eb6bfbe0b97
MEANPARK <techrosseo@naver.com>
2025-03-08 21:17:31 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int x=1;
+        while(true){
+            int y = (brown/2)-x+2;
+            if(x*y-brown==yellow){
+                return new int[]{y,x};
+            }
+            x++;
+        }
+    }
+}
