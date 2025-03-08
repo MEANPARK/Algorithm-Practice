98280135c04482173582709c9052031db2d26067
MEANPARK <techrosseo@naver.com>
2025-03-08 21:23:29 +0900

    [level 1] Title: 2016년, Time: 1.52 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        
+        LocalDate date = LocalDate.of(2016, a, b);
+        answer = date.getDayOfWeek().toString().substring(0,3);
+        
+        return answer;
+    }
+}
