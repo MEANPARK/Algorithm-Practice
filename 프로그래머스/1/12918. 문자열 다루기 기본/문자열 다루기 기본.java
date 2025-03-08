2d838f84eb168bf05a50125bc4019c3259d802fa
MEANPARK <techrosseo@naver.com>
2025-03-08 21:12:45 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,15 @@
+import java.util.regex.Pattern;
+
+class Solution {
+    public boolean solution(String s) {
+         if (s.length() == 4 || s.length() == 6) {
+            for (int i = 0; i < s.length(); i++) {
+                if (!Character.isDigit(s.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
