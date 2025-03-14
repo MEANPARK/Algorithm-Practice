7a14dc39ec03e738bc23f3a18dd81fe1f088bc96
MEANPARK <techrosseo@naver.com>
2025-03-14 17:28:12 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.78 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for(String key : keymap) {
+            int index = 1;
+            for(char c : key.toCharArray()) {
+                if(map.getOrDefault(c, 0) == 0) {
+                    map.put(c, index);
+                }
+                else if(map.get(c) > index) {
+                    map.put(c, index);
+                }
+                index++;
+            }
+        }
+        
+        int aindex = 0;
+        for(String target : targets) {
+            for(char c : target.toCharArray()) {
+                if(map.getOrDefault(c, 0) == 0) {
+                    answer[aindex] = 0;
+                    break;
+                }
+                else {
+                    answer[aindex] += map.get(c);
+                }
+            }
+            if(answer[aindex] == 0) answer[aindex] = -1;
+            aindex++;
+        }
+        return answer;
+    }
+}
