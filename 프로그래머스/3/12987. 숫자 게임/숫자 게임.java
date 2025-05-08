3a9929c3c7855e1e4654ae2d583c6166ecd1dab9
MEANPARK <techrosseo@naver.com>
2025-05-08 15:29:23 +0900

    [level 3] Title: 숫자 게임, Time: 109.08 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int index = 0;
+        for(int i = 0; i < A.length; i++) {
+            if(A[index] < B[i]) {
+                index++;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
